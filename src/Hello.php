a9fdf6733f0caf83c2b7707ea5b76aa800a8780e
fa0119 <chted119@gmail.com>
2019-07-02 13:25:01 +0800

    測試用package

---
+++ src/Hello.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace testpackage\testhello;
+
+class Hello {
+
+    // public function __construct($a, $b) {
+    //     $this->getHello = $a + $b;
+    // }
+
+    public function getHello() {
+        return print_r('Hello Test');
+    }
+}
---
+++ src/TestHelloServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace testpackage\testhello;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestHelloServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
